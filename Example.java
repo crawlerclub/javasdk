1cb99e4254117c44910e1d9b26a7d3e2d328641e
liuzhanliang <zhanliangliu@qq.com>
2018-06-01 11:52:14 +0800

    fix typo, http->https

--- Example.java
+++ Example.java
@@ -8,7 +8,7 @@ public class Example {
 		Client api = new Client("YOUR_ACCESS_KEY", "YOUR_SECRET_KEY");
 		Map<String, String> data = new HashMap<>();
 		data.put("number", "13702032331");
-		String url = "http://api.crawler.club/phone";
+		String url = "https://api.crawler.club/phone";
 		String response = api.Request(url, data);
 		System.out.println(response);
 	}
